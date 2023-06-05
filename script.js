c827a8c09814342eb212de0a65e60ef962b7570d
Shaun Vandermey <shaunyv132@outlook.com>
2023-06-05 12:11:07 +1000

    Add initial number and operations buttons as well as basic styling.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let operation = "none";
+let previousArg = null;
+let currentArg = null;
+
+
+//use the previous calculation, and evaluate a new addition
+function addArg(prevArg, currArg){
+
+}
+
+function subtractArg(prevArg, currArg){
+
+}
+
+function multiplyArg(prevArg, currArg){
+
+}
+
+function divideArg(prevArg, currArg){
+
+}
