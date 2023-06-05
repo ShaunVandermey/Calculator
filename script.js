b5d68562aef2598dacf6f59bb1e25f8f4ff346ad
Shaun Vandermey <shaunyv132@outlook.com>
2023-06-05 18:53:02 +1000

    Add in main operation functionality and evaluations.

--- script.js
+++ script.js
@@ -1,22 +1,113 @@
 
-let operation = "none";
-let previousArg = null;
-let currentArg = null;
-
+let operation = null;
+let previousNumber = null;
+let currentNumber = null;
+let firstComplete = false;
 
 //use the previous calculation, and evaluate a new addition
-function addArg(prevArg, currArg){
-
+function addArg(){
+    operation = "+";
+    firstComplete = true;
+    displayString();
 }
 
-function subtractArg(prevArg, currArg){
-
+function subtractArg(){
+    operation = "-";
+    firstComplete = true;
+    displayString();
 }
 
-function multiplyArg(prevArg, currArg){
-
+function multiplyArg(){
+    operation = "*";
+    firstComplete = true;
+    displayString();
 }
 
-function divideArg(prevArg, currArg){
+function divideArg(){
+    operation = "/";
+    firstComplete = true;
+    displayString();
+}
+
+function appendNumber(num){
+    
+    if(!firstComplete){
+        if(previousNumber == null || previousNumber == "0"){
+            previousNumber = num;
+        }
+        else{
+            previousNumber += num;
+        }
+    }
+    else{
+        if(currentNumber == null || currentNumber == "0"){
+            currentNumber = num;
+        }
+        else{
+            currentNumber += num;
+        }
+    }
+    displayString();
+}
+
+function displayString(){
+    if(previousNumber != null){
+        displayText.textContent = previousNumber;
+    }
+    if(operation != null){
+        displayText.textContent += " " + operation + " ";
+    }
+    if(currentNumber != null){
+        displayText.textContent += currentNumber;
+
+    }
+}
+
+function evaluate(){
+    let result = 0;
+    if(previousNumber != null && operation != null && currentNumber != null){
+        switch(operation){
+            case "+":
+                result = parseFloat(previousNumber) + parseFloat(currentNumber);
+                break;
+            case "-":
+                result = parseFloat(previousNumber) - parseFloat(currentNumber);
+                break;
+            case "*":
+                result = parseFloat(previousNumber) * parseFloat(currentNumber);
+                break;
+            case "/":
+                result = parseFloat(previousNumber) / parseFloat(currentNumber);
+                break;
+        }
+        displayText.textContent = result;
+        previousNumber = result;
+        operation = null;
+        currentNumber = null;
+    }
+}
+
+const addBtn = document.getElementById("addition");
+addBtn.addEventListener("click", addArg);
+const minusBtn = document.getElementById("subtraction");
+minusBtn.addEventListener("click", subtractArg);
+const timesBtn = document.getElementById("multiplication");
+timesBtn.addEventListener("click", multiplyArg);
+const divideBtn = document.getElementById("division");
+divideBtn.addEventListener("click", divideArg);
+const equalsBtn = document.getElementById("equals");
+equalsBtn.addEventListener("click", evaluate);
+
+const numberBtns = document.getElementsByClassName("number");
+//numberBtns.forEach((button) =>
+    //button.addEventListener("click", () => appendNumber(button.textContent))
+    //);
+for(let i = 0; i < numberBtns.length; i++){
+    numberBtns[i].addEventListener("click", () => appendNumber(numberBtns[i].textContent));
+}
+
+const displayText = document.getElementById("displayArea");
+
+function setup(){
 
 }
